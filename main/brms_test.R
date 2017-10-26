1a844764b0ecea34de6d23bfca92ec0e793b21ca
Zhao <jzhao@imshealth.com>
2017-10-26 11:16:55 +0800

    use brms_test.R to test brm model 1. add resultDir 2.using rooted data 3. get the fixed and randomized coefficients

--- main/brms_test.R
+++ main/brms_test.R
@@ -28,18 +28,23 @@ n.cpu <- 4
 
 
 library(brms)
-model_data_list=readRDS(file = paste0('C:/work/working materials/MCM/R part/Code/Results/2017-10-17 07.22.03/', 'model_data_list.RDS'))
 nrx_var <- c('prescriptions')
+bStd <- T
+promo_var <- c("call", "meeting_epu", "meeting_national", "meeting_international", "meeting_other")
+
+model_data_list=readRDS(file = paste0('C:/work/working materials/MCM/R part/Code/Results/2017-10-17 07.22.03/', 'model_data_list.RDS'))
 
 IDs_vct <- model_data_list$mod_data4BaseLine$final_segment
-X4Bayes=model_data_list$X4Bayes
+# X4Bayes=model_data_list$X4Bayes
+# use data for baseline to do the stan model(the data adding root)
+prefix <- "_adj_stk_rt"
+X4Bayes <- model_data_list$mod_data4BaseLine[, paste0(promo_var, prefix)]
 model_data4BaseLine=model_data_list$mod_data4BaseLine
 ctrl_var_inBys <- c('log_trend', 'pos', 'neg', 'pos_77')
 # final_segment <- IDs_var
+IDs_var <- 'final_segment'
+
 ctrl_var=ctrl_var_inBys
-rnd_vars<- ctrl_var
-fix_vars_rnd <- promo_var_size_adj
-# rnd_vars = ""
 ctrl <- model_data4BaseLine[, ctrl_var]
 
 #       if(bStd){
@@ -57,37 +62,81 @@ IDs <- length(unique(model_data4BaseLine[, IDs_var]))
 
 data4brms <- cbind(y1, X)
 
-fix_vars <- paste0(promo_var, '_adj_stk')
-formula4brms <- as.formula(paste0('y1~'
-                  , paste0(fix_vars, collapse="+")
-                  , '+(1+'
-                  # , '+(1'
-                  , paste0(rnd_vars, collapse = "+")
-                  , "|"
-                  , IDs_var
-                  , ")"
-                  )
-                  )
-brmsformula(formula4brms)
+fix_vars <- c(paste0(promo_var, prefix)
+              ,  ctrl_var
+)
+rnd_vars <- c(paste0(promo_var, prefix), ctrl_var)
 
+library(dplyr)
+mu_prec_priors <- data.frame(
+      varNm=c(paste0(promo_var, prefix), ctrl_var)
+      , mu=c(0.040105695,	0.005538787,	0.012136681,	0.023471172,	0.001431081, 0,0,0,0)
+      , prec=c(1061.49395,	125222.6303,	26080.28162,	6973.368051,	1875787.238, 0.9604, 0.9604, 0.9604, 0.9604)
+      
+) %>%
+      mutate(vars=1/prec) %>%
+      mutate(stdev=vars^0.5) %>%
+      mutate(varNm=as.character(varNm))
+
+# %>% 
+#       t(.) %>%
+#       setNames(., c(promo_var, ctrl_var))
+getMu <- function(distri, var, idx1, idx2){
+      if(is.character(idx1)){
+            return(paste0(distri, "(", mu_prec_priors[match(var, mu_prec_priors$varNm), idx1], ","
+                          , mu_prec_priors[match(var, mu_prec_priors$varNm), idx2]
+                          , ')'))
+            
+      }else{
+            return(paste0(distri, "(", idx1, ","
+                          , mu_prec_priors[match(var, mu_prec_priors$varNm), idx2]
+                          , ')'))
+            
+      }
+      
+}
+
+# getMu('normal', 'call_adj_stk', 'mu', 'stdev')
+# names(mu_prec_priors)
 t0 <- Sys.time()
 brm_fit <- brm(formula = 
-                     # y1 ~ call_adj_stk+ (1+call_adj_stk | final_segment)
-               brmsformula(formula4brms)
+                     brmsformula(formula4brms)
                , data = data4brms
                # , family = lognormal()
-               , prior =  c(set_prior("normal(0.01, 0.05)", coef="call_adj_stk")
-                            , set_prior("normal(0.04, 0.05)", coef="meeting_epu_adj_stk")
-                            , set_prior("normal(0.012, 0.05)", coef="meeting_national_adj_stk")
-                            , set_prior("normal(0.023, 0.05)", coef="meeting_international_adj_stk")
-                            , set_prior("normal(0.001, 0.05)", coef="meeting_other_adj_stk")
+               , prior =  c(set_prior('normal(0, 1)', class = 'Intercept')
+                            , set_prior("cauchy(0, 2.5)", coef = 'Intercept', class='sd', group = IDs_var)
+                            
+                            , set_prior(getMu('normal', paste0("call", prefix), 'mu', 'stdev'), coef=paste0("call", prefix), class = 'b')
+                            , set_prior(getMu('normal', paste0("meeting_epu", prefix), 'mu', 'stdev'), coef=paste0("meeting_epu", prefix), class = 'b')
+                            , set_prior(getMu('normal', paste0("meeting_national", prefix), 'mu', 'stdev'), coef=paste0("meeting_national", prefix), class = 'b')
+                            , set_prior(getMu('normal', paste0("meeting_international", prefix), 'mu', 'stdev'), coef=paste0("meeting_international", prefix), class = 'b')
+                            , set_prior(getMu('normal', paste0("meeting_other", prefix), 'mu', 'stdev'), coef=paste0("meeting_other", prefix), class = 'b')
+                            
+                            , set_prior(getMu('cauchy', paste0("call", prefix), 'stdev', 2.5), coef=paste0("call", prefix), class='sd', group = IDs_var)
+                            , set_prior(getMu('cauchy', paste0("meeting_epu", prefix),  'stdev', 2.5), coef=paste0("meeting_epu", prefix), class='sd', group = IDs_var)
+                            , set_prior(getMu('cauchy', paste0("meeting_national", prefix),  'stdev', 2.5), coef=paste0("meeting_national", prefix), class='sd', group = IDs_var)
+                            , set_prior(getMu('cauchy', paste0("meeting_international", prefix),  'stdev', 2.5), coef=paste0("meeting_international", prefix), class='sd', group = IDs_var)
+                            , set_prior(getMu('cauchy', paste0("meeting_other", prefix),  'stdev', 2.5), coef=paste0("meeting_other", prefix), class='sd', group = IDs_var)
+                            
                )
                # , prior = prior_list
                , iter = 2000
                , chains = 3
-               , control = list(adapt_delta = 0.96)
+               # , control = list(adapt_delta = 0.96)
 )
-summary(brm_fit)
+summary_fit <- summary(brm_fit)
+
+
+timeStamp <- as.character(Sys.time())
+timeStamp <- gsub(":", ".", timeStamp)  # replace ":" by "."
+resultDir <- paste("./Results/", timeStamp, "/", sep = '')
+dir.create(resultDir, showWarnings = TRUE, recursive = TRUE, mode = "0777")
+
+fix <- fixef(brm_fit, old = T)
+write.csv(fix,paste0(resultDir, "fixed_from_brm.csv"))
+rnd <- ranef(brm_fit, old = T)
+write.csv(rnd, paste0(resultDir, "rnd_from_brm.csv"))
+saveRDS(summary_fit, file=paste0(resultDir, 'summary_brmFit.RDS'))
 
 timeUsed <- (Sys.time()-t0)
 timeUsed
