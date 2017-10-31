3cdcb096e1ea303f69756e9003fd905eb092eef0
Zhao <jzhao@imshealth.com>
2017-10-31 15:03:25 +0800

    clean up the coefficients from winbugs so that it is convernient to do comparison with that from brms

---
+++ main/summarize_coefs_winbugs.R
@@ -0,0 +1,40 @@
+# clean up the results from winbugss
+
+beta <- read.csv(file = "C:\\work\\working materials\\MCM\\R part\\Code\\Results\\2017-10-27 16.49.15\\prolia_Means_1.csv")
+
+names(beta) <- tolower(names(beta))
+
+bBeta <- grepl("^beta\\[", as.character(beta$x), ignore.case = T)
+
+beta_using <- beta[bBeta, ]
+
+temp_list <- lapply(1:86, function(s){
+      coef <- beta_using$mean
+      id_coefThisSeg <- (8*(s-1)+1):(8*(s-1)+8)
+      coefThisSeg <- coef[id_coefThisSeg]
+      return(coefThisSeg)
+})
+
+ctrl_var_inBys <- c('log_trend', 'pos', 'neg')
+
+coef_df <- do.call(rbind.data.frame, temp_list)
+
+names(coef_df) <- c(paste0(promo_var, prefix), ctrl_var_inBys)
+
+# fixed
+bBetam <- grepl("^betam\\[", as.character(beta$x), ignore.case = T)
+betam_using <- beta[bBetam, ]
+coef_agg <- betam_using$mean
+
+# rbind
+coef_df_fix_rnd <- rbind(coef_agg, coef_df)
+coef_df_fix_rnd$final_segment <- c("Aggregated", 1:86) 
+coef_df_fix_rnd <- select(coef_df_fix_rnd, one_of(c('final_segment', names(coef_df))))
+
+timeStamp <- as.character(Sys.time())
+timeStamp <- gsub(":", ".", timeStamp)  # replace ":" by "."
+resultDir <- paste("./Results/", timeStamp, "/", sep = '')
+dir.create(resultDir, showWarnings = TRUE, recursive = TRUE, mode = "0777")
+
+
+write.csv(coef_df_fix_rnd, file=paste0(resultDir, 'coef_fixAndRnd_winbugs.csv'), row.names = F)
